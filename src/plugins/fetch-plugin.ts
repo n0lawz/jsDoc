c3fb8b41168b7f58fda4b6e3d8c666baa9bc7270
Nat <nlaw.dev@gmail.com>
2022-04-06 15:53:56 -0700

    edited fetch plugin to allow for fetching of css files. only works with vanilla functions, some css features wont work

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -22,19 +22,29 @@ export const fetchPlugin = (inputCode: string) => {
         
             // Check to see if we have already fetched this file
             // and if it is in the cache
-            const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
+            // const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path);
         
-            // if it is, return it immediately
+            // // if it is, return it immediately
         
-            if (cachedResult) {
-              return cachedResult;
-            }
+            // if (cachedResult) {
+            //   return cachedResult;
+            // }
         
             const { data, request } = await axios.get(args.path);
-        
+            
+            const fileType = args.path.match(/.css$/) ? 'css' : 'jsx';
+
+            const contents = fileType === 'css' ? 
+            `
+            const style = document.createElement('style');
+            style.innerText = 'body { background-color: "red" }';
+            document.head.appendChild(style);
+            `
+            : data;
+
             const result: esbuild.OnLoadResult = {
               loader: 'jsx',
-              contents: data,
+              contents,
               resolveDir: new URL('./', request.responseURL).pathname,
             };
         
