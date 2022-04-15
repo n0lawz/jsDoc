86c31dce267ad09f5335ba0e992d664cdb5842b1
Nat <nlaw.dev@gmail.com>
2022-04-15 15:25:13 -0700

    so it does work. I added onMount interface from the type def file

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,8 @@
-import Editor from '@monaco-editor/react';
+import Editor, { OnMount } from '@monaco-editor/react';
 import { useRef } from 'react';
+import prettier from 'prettier';
+import parser from 'prettier/parser-babel';
+
 
 interface CodeEditorProps {
     defaultValue: string,
@@ -9,29 +12,38 @@ interface CodeEditorProps {
 const CodeEditor: React.FC<CodeEditorProps> = ({ onChange,defaultValue }) => {
     const editorRef = useRef<any>();
 
-    const onEditorMount = (editor: any, _monaco: any) => {
+    const onEditorMount: OnMount = (editor, _monaco) => {
         editorRef.current = editor;
         editor.onDidChangeModelContent(() => onChange(editor.getValue()));
         editor.getModel()?.updateOptions({ tabSize: 2 })
     };
 
-    return <Editor
-        onMount={onEditorMount}
-        height="500px"
-        language="javascript"
-        defaultValue={ defaultValue }
-        theme="vs-dark"
-        options={{
-            wordWrap: 'on',
-            minimap: {enabled: false},
-            showUnused: false,
-            folding: false,
-            lineNumbersMinChars: 3,
-            fontSize: 16,
-            scrollBeyondLastLine: false,
-            automaticLayout: true
-        }}
-    />;
+    const onFormatClick = () => {
+        // get current value from editor
+    }   
+
+    return (
+        <div>
+            <button onClick={onFormatClick}>Format</button>
+        <Editor
+            onMount={onEditorMount}
+            height="500px"
+            language="javascript"
+            defaultValue={ defaultValue }
+            theme="vs-dark"
+            options={{
+                wordWrap: 'on',
+                minimap: {enabled: false},
+                showUnused: false,
+                folding: false,
+                lineNumbersMinChars: 3,
+                fontSize: 16,
+                scrollBeyondLastLine: false,
+                automaticLayout: true
+            }}
+        />
+        </div>
+    );
 };
 
 export default CodeEditor;
