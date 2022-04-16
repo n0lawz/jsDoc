37b96b02ce97dc989601890a74e97d8178b1abb4
Nat <nlaw.dev@gmail.com>
2022-04-15 19:50:15 -0700

    extracted preview logic from index.tsx into seperate component

---
+++ src/components/preview.tsx
@@ -0,0 +1,44 @@
+import { useEffect, useRef } from 'react';
+
+interface PreviewProps {
+    code: string;
+}
+
+const html = `
+    <html>
+        <head></head>
+        <body>
+            <div id="root"></div>
+            <script>
+                window.addEventListener('message', (event) => {
+                    try{
+                        eval(event.data);
+                    } catch(err) {
+                        const root = document.querySelector('#root');
+                        root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
+                        console.error(err);
+                    }
+                }, false); 
+            </script>
+        </body>
+    </html>
+`
+
+const Preview: React.FC<PreviewProps> = ({ code }) => { 
+    const iframe = useRef<any>();
+
+    useEffect(() => {
+        // clears iframe content before next render
+        iframe.current.srcdoc = html;
+
+        // join together all the output files of the bundle
+        // updates code piece of state where the output of our bundle is stored
+        // setCode(result.outputFiles[0].text);
+        iframe.current.contentWindow.postMessage(code, '*');
+    }, [code]);
+
+
+    return <iframe ref={iframe} title="code-preview" sandbox="allow-scripts" srcDoc={html} />;
+};
+
+export default Preview;
--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,12 @@ import ReactDOM from 'react-dom/client';
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
 import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
+import Preview from './components/preview';
 
 
 const App = () => {
     const ref = useRef<any>();
-    const iframe = useRef<any>();
+    const [code, setCode] = useState('');
     // code the user writes into text area
     const [input, setInput] = useState('');
 
@@ -33,9 +34,6 @@ const App = () => {
             return;
         }
 
-        // clears iframe content before next render
-        iframe.current.srcdoc = html;
-
         // what we get back from onClick
         const result = await ref.current.build({
             entryPoints: ['index.js'],
@@ -48,33 +46,8 @@ const App = () => {
             }
         });
 
-         // join together all the output files of the bundle
-         // updates code piece of state where the output of our bundle is stored
-        // setCode(result.outputFiles[0].text);
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*');
-
+        setCode(result.outputFiles[0].text);
     };
-
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    window.addEventListener('message', (event) => {
-                        try{
-                            eval(event.data);
-                        } catch(err) {
-                            const root = document.querySelector('#root');
-                            root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
-                            console.error(err);
-                        }
-                    }, false); 
-                </script>
-            </body>
-        </html>
-    `
-
 
     return (
         <div>
@@ -82,11 +55,10 @@ const App = () => {
                 defaultValue='Type code here'
                 onChange={(value) => setInput(value)}
             />
-            <textarea value ={input} onChange={e => setInput(e.target.value)}></textarea>
             <div>
                 <button onClick={onClick}>Submit</button>
             </div>
-            <iframe  ref={iframe} title="code-preview" sandbox="allow-scripts" srcDoc={html} />
+            <Preview code={code} />
         </div>
     )
 };
