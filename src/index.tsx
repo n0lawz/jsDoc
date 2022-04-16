47b23a652f6037007c8adeba5449648eaa1bfc8e
Nat <nlaw.dev@gmail.com>
2022-04-15 20:19:19 -0700

    extracted bundling logic into seperate file

--- src/index.tsx
+++ src/index.tsx
@@ -1,66 +1,35 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
-import * as esbuild from 'esbuild-wasm';
-import { useState, useEffect, useRef } from 'react';
+import { useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin';
-import { fetchPlugin } from './plugins/fetch-plugin';
 import CodeEditor from './components/code-editor';
 import Preview from './components/preview';
-
+import bundle from './bundler';
 
 const App = () => {
-    const ref = useRef<any>();
     const [code, setCode] = useState('');
     // code the user writes into text area
     const [input, setInput] = useState('');
 
-    // initializing esbuild by fetcbj g esbuild.wasm binary from unpkg
-    // used useRef hook to have access outside of startService function
-    const startService = async () => {
-        ref.current = await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-        });
-    };
-
-    // calling startService once when we start our application
-    useEffect(() => {
-        startService();
-    }, []);
-
-    // function that builds bundle and stores it in result
+    // function that takes input from user and bundles
+    // we use setCode to update state
     const onClick = async () => {
-        if (!ref.current) {
-            return;
-        }
-
-        // what we get back from onClick
-        const result = await ref.current.build({
-            entryPoints: ['index.js'],
-            bundle: true,
-            write: false,
-            plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-            define: {
-                'process.env.NODE_ENV': '"production"',
-                global: 'window',
-            }
-        });
-
-        setCode(result.outputFiles[0].text);
+        const output = await bundle(input);
+        setCode(output);
     };
 
-    return (
-        <div>
-            <CodeEditor 
-                defaultValue='Type code here'
-                onChange={(value) => setInput(value)}
-            />
+        return (
             <div>
-                <button onClick={onClick}>Submit</button>
+                <CodeEditor 
+                    defaultValue='Type code here'
+                    onChange={(value) => setInput(value)}
+                />
+                <div>
+                    <button onClick={onClick}>Submit</button>
+                </div>
+                <Preview code={code} />
             </div>
-            <Preview code={code} />
-        </div>
-    )
+        );
+    
 };
 
 // this is how we render in react 18. replaced reactdom.render()
