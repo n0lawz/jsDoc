b5c4936d5d8afe33ed9982981f346209c9863875
Nat <nlaw.dev@gmail.com>
2022-04-18 22:03:46 -0700

    updating error handling to handle async errors

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -1,29 +1,35 @@
 import "./preview.css";
 import { useEffect, useRef } from "react";
-import { setTokenSourceMapRange } from "typescript";
 
 interface PreviewProps {
   code: string;
 }
 
 const html = `
-    <html>
-        <head></head>
-        <body>
-            <div id="root"></div>
-            <script>
-                window.addEventListener('message', (event) => {
-                    try{
-                        eval(event.data);
-                    } catch(err) {
-                        const root = document.querySelector('#root');
-                        root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
-                        console.error(err);
-                    }
-                }, false); 
-            </script>
-        </body>
-    </html>
+  <html>
+    <head></head>
+      <body>
+        <div id="root"></div>
+          <script>
+            const handleError = (err) => {
+              const root = document.querySelector('#root');
+              root.innerHTML = '<div style="color: red"><h4>Runtime Error</h4>' + err + '</div>';
+              console.error(err);
+            };
+
+            window.addEventListener('error', (event) => {
+              handleError(event.error);
+            })
+            window.addEventListener('message', (event) => {
+              try {
+                eval(event.data);
+              } catch(err) {
+                handleError(err)
+              }
+            }, false); 
+          </script>
+      </body>
+  </html>
 `;
 
 const Preview: React.FC<PreviewProps> = ({ code }) => {
@@ -32,7 +38,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
   useEffect(() => {
     // clears iframe content before next render
     iframe.current.srcdoc = html;
-    
+
     // join together all the output files of the bundle
     // updates code piece of state where the output of our bundle is stored
     // setCode(result.outputFiles[0].text);
