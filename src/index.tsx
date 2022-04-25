c51de8fc71793060611e8ccb5110a26890aefd93
Nat <nlaw.dev@gmail.com>
2022-04-25 02:02:45 -0700

    added in action creators and exported each respective interface

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
 // import CodeCell from "./components/code-cell";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./redux";
 import TextEditor from "./components/text-editor";
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <TextEditor />
+      </div>
+    </Provider>
   );
 };
 
---
+++ src/redux/action-creators/index.ts
@@ -0,0 +1,7 @@
+import { ActionType } from '../action-types';
+import { Action, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellBeforeAction } from '../actions';
+
+export const udpateCell = (): UpdateCellAction => {}
+export const deleteCell = (): DeleteCellAction => {};
+export const moveCell = (): MoveCellAction => {};
+export const insertCellBefore = (): InsertCellBeforeAction => {};
