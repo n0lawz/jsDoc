5944ee2ff120d2497567fbe36d11b967d4aadf4c
Nat <nlaw.dev@gmail.com>
2022-04-25 02:29:14 -0700

    udpated action creators. still need to create type for direction prop in moveCell

--- src/redux/action-creators/index.ts
+++ src/redux/action-creators/index.ts
@@ -1,7 +1,37 @@
 import { ActionType } from '../action-types';
 import { Action, UpdateCellAction, DeleteCellAction, MoveCellAction, InsertCellBeforeAction } from '../actions';
+import { CellTypes } from '../cell';
 
-export const udpateCell = (): UpdateCellAction => {}
-export const deleteCell = (): DeleteCellAction => {};
-export const moveCell = (): MoveCellAction => {};
-export const insertCellBefore = (): InsertCellBeforeAction => {};
+export const udpateCell = (id: string, content: string): UpdateCellAction => {
+  return {
+    type: ActionType.UPDATE_CELL,
+    payload: {
+      id,
+      content
+    }
+  }
+}
+export const deleteCell = (id: string): DeleteCellAction => {
+  return {
+    type: ActionType.DELETE_CELL,
+    payload: id
+  }
+};
+export const moveCell = (id: string, direction: 'up' | 'down'): MoveCellAction => {
+  return {
+    type: ActionType.MOVE_CELL,
+    payload: {
+      id,
+      direction
+    }
+  }
+};
+export const insertCellBefore = (id: string, cellType: CellTypes): InsertCellBeforeAction => {
+  return {
+    type: ActionType.INSERT_CELL_BEFORE,
+    payload: {
+      id,
+      type: cellType
+    }
+  }
+};
