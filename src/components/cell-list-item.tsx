ac9c985b2907c5ddaf472301ee9938ea976daa7b
Nat <nlaw.dev@gmail.com>
2022-04-26 16:29:50 -0700

    we can definetly read state out of our redux store. need to add buttons to dispatch new cell actions

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,9 +1,17 @@
 import { Cell } from '../redux';
+import CodeCell from './code-cell';
+import TextEditor from './text-editor';
 interface CellListItemProps {
   cell: Cell;
 }
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
-  return <div>{cell.id}</div>;
+  let child: JSX.Element;
+  if (cell.type === 'code') {
+    child = <CodeCell />
+  } else {
+    child = <TextEditor />
+  }
+  return <div>{child}</div>;
 };
 
 export default CellListItem;
