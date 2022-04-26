dcb152a6ed77bd90ed6b27f537ead67089ff83c6
Nat <nlaw.dev@gmail.com>
2022-04-26 01:14:01 -0700

    set up cell list item and cell list component. need to test

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,5 +1,9 @@
-const CellListItem: React.FC = () => {
-  return <div>Cell List Item</div>;
+import { Cell } from '../redux';
+interface CellListItemProps {
+  cell: Cell;
+}
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  return <div>{cell.id}</div>;
 };
 
 export default CellListItem;
