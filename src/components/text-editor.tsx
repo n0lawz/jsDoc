2f3ea21a8be1e88c43feccff03ad03fd1e00dd25
Nat <nlaw.dev@gmail.com>
2022-04-26 17:32:30 -0700

    updated text editor to take in updatecell action

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,12 +1,18 @@
 import './text-editor.css';
-import MDEditor, { MDEditorProps } from "@uiw/react-md-editor";
+import MDEditor from "@uiw/react-md-editor";
 import React, { useState, useEffect, useRef } from "react";
+import { Cell } from '../redux';
+import { useActions } from '../hooks/use-actions';
 
+interface TextEditorProps {
+  cell: Cell;
+}
 
-const TextEditor: React.FC<MDEditorProps> = () => {
+const TextEditor: React.FC<TextEditorProps> = ({ cell }) => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
   const [value, setValue] = useState<string>('hi');
+  const { updateCell } = useActions();
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -27,14 +33,14 @@ const TextEditor: React.FC<MDEditorProps> = () => {
   if (editing) {
     return (
       <div data-color-mode="dark" className='text-editor' ref={ref}>
-        <MDEditor value={value} onChange={(value?: string | undefined) => setValue(value!)} />
+        <MDEditor value={cell.content} onChange={(value?: string | undefined) => updateCell(cell.id, value || '')} />
       </div>
     );
   }
   return (
     <div className="text-editor card" onClick={() => setEditing(true)}>
       <div className="card-content">
-        <MDEditor.Markdown source={value} />
+        <MDEditor.Markdown source={cell.content} />
       </div>
     </div>
   ); 
