a0bf9cef1465d2ea56dacb37822fbd2cc4dd14ec
Nat <nlaw.dev@gmail.com>
2022-05-03 18:34:39 -0700

    started to set up bundling reducer

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -12,14 +12,16 @@ const CellList: React.FC = () => {
 
   const renderedCells = cells.map((cell) => (
     <Fragment key={cell.id}>
-      <AddCell nextCellId={cell.id} />
       <CellListItem key={cell.id} cell={cell} />
+      <AddCell previousCellId={cell.id} />
     </Fragment>
   )); 
-  return <div>
+  return (
+    <div>
+      <AddCell previousCellId={null} />
       {renderedCells}
-        <AddCell nextCellId={null} />
-  </div>
+    </div>
+  );
 };
 
 export default CellList;
