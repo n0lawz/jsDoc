c7559aa439d28e682cf69d831e61d9b52322a28c
Nat <nlaw.dev@gmail.com>
2022-05-03 18:48:57 -0700

    finished bundlereducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,8 +1,9 @@
 import cellsReducer from './cellsReducer';
 import { combineReducers } from 'redux';
-
+import bundlesReducer from './bundlesReducer';
 const reducers = combineReducers({
-  cells: cellsReducer
+  cells: cellsReducer,
+  bundles: bundlesReducer
 });
 
 export default reducers;
