aeae25b12b1b020e0771f45b441d7edb7e240965
Nat <nlaw.dev@gmail.com>
2022-05-05 00:43:53 -0700

    altered cumulativecode so we show preview only in current code window

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -17,11 +17,10 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
     const { data, order } = state.cells;
     const orderedCells = order.map(id => data[id]);
 
-    const cumulativeCode = [
-      `
+    const showFunc = `
         import _React from 'react';
         import _ReactDOM from 'react-dom';
-        const show = (value) => {
+        var show = (value) => {
           const root = document.querySelector('#root');
           if (typeof value === 'object') {
             if (value.$$typeof && value.props) {
@@ -33,10 +32,17 @@ const CodeCell: React.FC<CodeCellProps> = ({ cell }) => {
             root.innerHTML = value;
           }
         };
-      `,
-    ];
+      `;
+    const showFuncNoop = 'var show = () => {}';
+
+    const cumulativeCode = [];
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        if (c.id === cell.id) {
+          cumulativeCode.push(showFunc);
+        } else {
+          cumulativeCode.push(showFuncNoop)
+        }
         cumulativeCode.push(c.content);
       }
       if (c.id === cell.id) {
