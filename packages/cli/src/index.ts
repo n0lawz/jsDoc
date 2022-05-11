6d9c22c60b1c84bf0dfd1bb4438893fa96d86aba
Nat <nlaw.dev@gmail.com>
2022-05-10 17:52:54 -0700

    initial commit

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,8 @@
-import serve from 'local-api';
+#!/usr/bin/env node
+import { program } from 'commander';
+import { serveCommand } from './commands/serve';
 
-serve();
+program
+  .addCommand(serveCommand);
+
+  program.parse(process.argv);
